3d02218d4855d384608ff4f1754bb6faeb491958
canewsin <canews.in@gmail.com>
2022-09-27 00:56:06 +0530

    Use assert instead of print

--- example/main.dart
+++ example/main.dart
@@ -1,15 +1,12 @@
-import 'package:zeronet_ws/extensions/futures.dart';
 import 'package:zeronet_ws/zeronet_ws.dart';
 
 main(List<String> args) async {
   await ZeroNet.instance.connect('1HELLoE3sFD9569CLCbHEAVqvqV7U2Ri9d');
   var infoMsg = await ZeroNet.instance.siteInfoFuture();
-  print(infoMsg.cmd);
-  print(infoMsg.id);
-  print(infoMsg.result);
+  assert(infoMsg.cmd == 'siteInfo');
+  assert(infoMsg.result != null);
 
   var annInfo = await ZeroNet.instance.announcerInfoFuture();
-  print(annInfo.cmd);
-  print(annInfo.id);
-  print(annInfo.result);
+  assert(annInfo.cmd == 'announcerInfo');
+  assert(annInfo.result != null);
 }
