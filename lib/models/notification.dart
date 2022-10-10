71d1e74a00dc4350f1a3d1cf77d5e7f7cf566ba7
canewsin <canews.in@gmail.com>
2022-10-11 02:55:11 +0530

    remove cmd from Notification

--- lib/models/notification.dart
+++ lib/models/notification.dart
@@ -1,20 +1,17 @@
 part of 'models.dart';
 
 class Notification {
-  final String cmd;
   final List<String> params;
   final int id;
 
-  Notification(this.cmd, this.params, this.id);
+  Notification(this.params, this.id);
 
   factory Notification.fromJson(Map<String, dynamic> json) => Notification(
-        json['cmd'] as String,
         json['params'] as List<String>,
         json['id'] as int,
       );
 
   Map<String, dynamic> toJson() => {
-        'cmd': cmd,
         'params': params,
         'id': id,
       };
