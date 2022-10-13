50454e8ccb8baba81a6a72bb6a703f1e5d207d30
canewsin <canews.in@gmail.com>
2022-10-14 02:59:41 +0530

    models:: ServerInfo field type correction

--- lib/models/serverinfo.dart
+++ lib/models/serverinfo.dart
@@ -1,95 +1,96 @@
 part of 'models.dart';
 
 class ServerInfo extends Result {
-  bool? ipExternal;
-  PortOpened? portOpened;
-  String? platform;
-  String? fileserverIp;
-  int? fileserverPort;
-  bool? torEnabled;
-  String? torStatus;
-  bool? torHasMeekBridges;
-  bool? torUseBridges;
+  String version;
+  int rev;
+  String platform;
+
   String? uiIp;
   int? uiPort;
-  String? version;
-  int? rev;
+  String? fileserverIp;
+  int? fileserverPort;
+  PortOpened portOpened;
+  bool? ipExternal;
+
   double? timecorrection;
   String? language;
   bool? debug;
   bool? offline;
-  List<String>? plugins;
-  PluginsRev? pluginsRev;
-  UserSettings? userSettings;
+
+  bool torEnabled;
+  String? torStatus;
+  bool torHasMeekBridges;
+  bool? torUseBridges;
+
+  List<String> plugins;
+  PluginsRev pluginsRev;
+  UserSettings userSettings;
+
   String? updatesite;
   String? distType;
   String? libVerifyBest;
 
   ServerInfo({
-    this.ipExternal,
-    this.portOpened,
-    this.platform,
+    required this.version,
+    required this.rev,
+    required this.platform,
+    //
+    required this.ipExternal,
+    required this.portOpened,
+    required this.torEnabled,
+    required this.torHasMeekBridges,
+    required this.plugins,
+    required this.pluginsRev,
     this.fileserverIp,
     this.fileserverPort,
-    this.torEnabled,
     this.torStatus,
-    this.torHasMeekBridges,
     this.torUseBridges,
     this.uiIp,
     this.uiPort,
-    this.version,
-    this.rev,
     this.timecorrection,
     this.language,
     this.debug,
     this.offline,
-    this.plugins,
-    this.pluginsRev,
-    this.userSettings,
+    required this.userSettings,
     this.updatesite,
     this.distType,
     this.libVerifyBest,
   });
 
-  ServerInfo.fromJson(Map<String, dynamic> json) {
-    ipExternal = json['ip_external'];
-    portOpened = json['port_opened'] != null
-        ? PortOpened.fromJson(json['port_opened'])
-        : null;
-    platform = json['platform'];
-    fileserverIp = json['fileserver_ip'];
-    fileserverPort = json['fileserver_port'];
-    torEnabled = json['tor_enabled'];
-    torStatus = json['tor_status'];
-    torHasMeekBridges = json['tor_has_meek_bridges'];
-    torUseBridges = json['tor_use_bridges'];
-    uiIp = json['ui_ip'];
-    uiPort = json['ui_port'];
-    version = json['version'];
-    rev = json['rev'];
-    timecorrection = json['timecorrection'];
-    language = json['language'];
-    debug = json['debug'];
-    offline = json['offline'];
-    plugins = json['plugins'].cast<String>();
-    pluginsRev = json['plugins_rev'] != null
-        ? PluginsRev.fromJson(json['plugins_rev'])
-        : null;
-    userSettings = json['user_settings'] != null
-        ? UserSettings.fromJson(json['user_settings'])
-        : null;
-    updatesite = json['updatesite'];
-    distType = json['dist_type'];
-    libVerifyBest = json['lib_verify_best'];
+  factory ServerInfo.fromJson(Map<String, dynamic> json) {
+    return ServerInfo(
+      version: json['version'],
+      rev: json['rev'],
+      platform: json['platform'],
+      ipExternal: json['ip_external'],
+      portOpened: PortOpened.fromJson(json['port_opened'] ?? {}),
+      torEnabled: json['tor_enabled'],
+      torHasMeekBridges: json['tor_has_meek_bridges'],
+      plugins: json['plugins'].cast<String>(),
+      pluginsRev: PluginsRev.fromJson(json['plugins_rev'] ?? {}),
+      fileserverIp: json['fileserver_ip'],
+      fileserverPort: json['fileserver_port'],
+      torStatus: json['tor_status'],
+      torUseBridges: json['tor_use_bridges'],
+      uiIp: json['ui_ip'],
+      uiPort: json['ui_port'],
+      timecorrection: json['timecorrection'],
+      language: json['language'],
+      debug: json['debug'],
+      offline: json['offline'],
+      userSettings: UserSettings.fromJson(json['user_settings'] ?? {}),
+      updatesite: json['updatesite'],
+      distType: json['dist_type'],
+      libVerifyBest: json['lib_verify_best'],
+    );
   }
 
   @override
   Map<String, dynamic> toJson() {
     final data = <String, dynamic>{};
     data['ip_external'] = ipExternal;
-    if (portOpened != null) {
-      data['port_opened'] = portOpened!.toJson();
-    }
+    data['port_opened'] = portOpened.toJson();
+
     data['platform'] = platform;
     data['fileserver_ip'] = fileserverIp;
     data['fileserver_port'] = fileserverPort;
@@ -106,12 +107,9 @@ class ServerInfo extends Result {
     data['debug'] = debug;
     data['offline'] = offline;
     data['plugins'] = plugins;
-    if (pluginsRev != null) {
-      data['plugins_rev'] = pluginsRev!.toJson();
-    }
-    if (userSettings != null) {
-      data['user_settings'] = userSettings!.toJson();
-    }
+    data['plugins_rev'] = pluginsRev.toJson();
+    data['user_settings'] = userSettings.toJson();
+
     data['updatesite'] = updatesite;
     data['dist_type'] = distType;
     data['lib_verify_best'] = libVerifyBest;
@@ -142,13 +140,19 @@ class PortOpened {
 }
 
 class PluginsRev {
-  PluginsRev();
+  final Map<String, int> rev;
+  PluginsRev(this.rev);
 
-  PluginsRev.fromJson(Map<String, dynamic>? json);
+  factory PluginsRev.fromJson(Map<String, dynamic> json) {
+    var map = <String, int>{};
+    for (final key in json.keys) {
+      map[key] = json[key];
+    }
+    return PluginsRev(map);
+  }
 
   Map<String, dynamic> toJson() {
-    final data = <String, dynamic>{};
-    return data;
+    return rev;
   }
 }
 
