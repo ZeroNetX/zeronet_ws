2e628d3bc11290394272ce157ec5cb2a6cfa69f7
canewsin <canews.in@gmail.com>
2022-10-15 02:21:20 +0530

    corsPermissionFuture: Fix Params & Return Type

--- lib/extensions/plugins/cors.dart
+++ lib/extensions/plugins/cors.dart
@@ -2,13 +2,13 @@ part of '../futures.dart';
 
 extension CorsExt on ZeroNet {
   ///Return: "ok" on success.
-  Future<Message> corsPermissionFuture(String address) async {
+  Future<MessageOrPrompt> corsPermissionFuture(List<String> address) async {
     var resultStr = await ZeroNet.instance.cmdFuture(
       ZeroNetCmd.corsPermission,
       params: {
         'address': address,
       },
     );
-    return resultStr.toMessage();
+    return resultStr.toMsgOrPrompt;
   }
 }
