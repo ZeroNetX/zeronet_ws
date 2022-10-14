5fccf8caaea6b0e9109d0b30715bd54661fd8613
canewsin <canews.in@gmail.com>
2022-10-15 02:17:58 +0530

    plugins::Chart Modify chartDbQueryFuture return type Modify chartGetPeerLocationsFuture return type

--- lib/extensions/plugins/chart.dart
+++ lib/extensions/plugins/chart.dart
@@ -1,16 +1,25 @@
 part of '../futures.dart';
 
 extension ChartExt on ZeroNet {
-  Future<Message> chartDbQueryFuture(String query, Map sqlparams) async {
-    var msg = await ZeroNet.instance.dbQueryFuture(query, sqlparams);
-    return msg;
+  Future<MessageOrError> chartDbQueryFuture(
+    String query, {
+    Map? sqlparams,
+  }) async {
+    final res = await ZeroNet.instance.cmdFuture(
+      ZeroNetCmd.chartDbQuery,
+      params: {
+        'query': query,
+        'params': sqlparams,
+      },
+    );
+    return res.toMsgOrErr;
   }
 
   ///Return: A list of unique peers.
   Future<Message> chartGetPeerLocationsFuture() async {
-    var msg = await ZeroNet.instance.cmdFuture(
+    final res = await ZeroNet.instance.cmdFuture(
       ZeroNetCmd.chartGetPeerLocations,
     );
-    return msg.toMessage();
+    return res.message!;
   }
 }
